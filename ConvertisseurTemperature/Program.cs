2b3e376e9dd35a05c428918fc4536a5f9a3bfa83
Jorge <jorgemanoel_s@hotmail.com>
2025-09-02 17:39:35 -0400

    Premier Commit

---
+++ ConvertisseurTemperature/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Programme
+{
+    static void Main()
+    {
+        // Variable pour stocker la température en Celsius
+        double temperatureCelsius;
+
+        Console.Write("Entrez la température en Celsius : ");
+        temperatureCelsius = Convert.ToDouble(Console.ReadLine());
+
+        // Conversion en Fahrenheit
+        double temperatureFahrenheit = (temperatureCelsius * 9 / 5) + 32;
+
+        // Conversion en Kelvin
+        double temperatureKelvin = temperatureCelsius + 273.15;
+
+        Console.WriteLine($"Température en Fahrenheit : {temperatureFahrenheit} °F");
+        Console.WriteLine($"Température en Kelvin : {temperatureKelvin} K");
+    }
+}
